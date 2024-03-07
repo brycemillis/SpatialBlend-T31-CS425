c33622ffd23b3ae154ee1a0370f3e838bb1cbb5a
Jose Diaz <josediaz@nevada.unr.edu>
2024-03-07 02:52:46 -0800

    Form for user image upload

--- src/baseapp/forms.py
+++ src/baseapp/forms.py
@@ -1,4 +1,3 @@
-from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
@@ -7,3 +6,7 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+class ImageUploadForm(forms.Form):
+    image1 = forms.ImageField(label='Image 1')
+    image2 = forms.ImageField(label='Image 2')
