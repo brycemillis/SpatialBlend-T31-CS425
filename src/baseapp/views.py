0eada079cf285578f93c6c620191590c2256246b
Jose Diaz <josediaz@nevada.unr.edu>
2024-03-07 02:56:05 -0800

    Added 'Learn More' view and 'image_combine_view' functionality

--- src/baseapp/views.py
+++ src/baseapp/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from io import BytesIO
 
+from PIL import Image
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+from django.shortcuts import render, redirect
 from django.contrib import messages
 
-from .forms import CreateUserForm
+from .forms import CreateUserForm, ImageUploadForm
+from .image_processing import create_hybrid_image
+
 
 def home_view(request):
     context = {}
@@ -15,8 +20,35 @@ def about_us_view(request):
     context = {}
     return render(request, "pages/aboutUs.html", context)
 
+def learn_more_view(request):
+    context = {}
+    return render(request, "pages/learnMore.html", context)
+
 def image_combine_view(request):
     context = {}
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            image1 = Image.open(request.FILES['image1'])
+            image2 = Image.open(request.FILES['image2'])
+            hybrid_image = create_hybrid_image(image1, image2, low_pass_cutoff=20, high_pass_cutoff=40)
+
+            # Convert PIL image to byte array
+            image_io = BytesIO()
+            hybrid_image.save(image_io, format='PNG')
+            image_io.seek(0)
+
+            # Save the hybrid image in Django's default storage (usually media folder)
+            filename = 'hybrid_images/hybrid_image_result.png'
+            default_storage.save(filename, ContentFile(image_io.getvalue()))
+
+            # Generate the URL for downloading
+            hybrid_image_url = default_storage.url(filename)
+            context['hybrid_image_url'] = hybrid_image_url
+    else:
+        form = ImageUploadForm()
+    
+    context['form'] = form
     return render(request, "pages/imageCombine.html", context)
 
 def image_editor_view(request):
