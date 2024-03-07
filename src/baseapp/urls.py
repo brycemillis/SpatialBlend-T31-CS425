64deba8e8a3f9714c60cfcb3a3ac9a0d0fc51184
Jose Diaz <josediaz@nevada.unr.edu>
2024-03-07 02:54:30 -0800

    Added 'Learn More' and password reset paths

--- src/baseapp/urls.py
+++ src/baseapp/urls.py
@@ -9,18 +9,16 @@ urlpatterns = [
     path('aboutUs/', views.about_us_view, name='about_us'),
     path('imageCombine/', views.image_combine_view, name='image_combine'),
     path('imageEditor/', views.image_editor_view, name='image_editor'),
+    path('learnMore/', views.learn_more_view, name='learn_more'),
     
     path('login/', views.login_view, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     
-    
     path('recentBlends/', views.recent_blends_view, name='recent_blends'),
     path('signup/', views.signup_view, name='signup'),
     
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name = "reset_passwrd"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name = "password_reset_done"),
-    path('reset_<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name = "password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name = "password_reset_complete"),
-    
-    # path(images)
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="pages/reset_password.html"), name = "reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="pages/reset_password_sent.html"), name = "password_reset_done"),
+    path('reset_<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="pages/reset_password_form.html"), name = "password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="pages/reset_password_complete.html"), name = "password_reset_complete"),
 ]
