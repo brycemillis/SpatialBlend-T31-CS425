b7b9f3fdd6eeaf556b9391541740e0094fea6d3e
brycemillis <brycemillis@gmail.com>
2024-04-24 17:09:43 -0700

    Added new custom css and added the my blends page
    Added recent blends page, made some necessary css changes, and edited some database functionality so that it will work properly

--- src/spatialblend/settings.py
+++ src/spatialblend/settings.py
@@ -72,26 +72,26 @@ TEMPLATES = [
 WSGI_APPLICATION = 'spatialblend.wsgi.application'
 
 
-# Database
+  # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'spatialblend',
-        'USER': 'postgres',
-        'PASSWORD':'',  # Enter your database password here 
-        'HOST':'localhost',
-        'POST':'5432'
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
+
+# DATABASES = {
+  #  'default': {
+  #      'ENGINE': 'django.db.backends.postgresql',
+   #     'NAME': 'spatialblend',
+   #     'USER': 'postgres',
+   #     'PASSWORD':'',  # Enter your database password here 
+    #    'HOST':'localhost',
+    #    'POST':'5432'
+  #  }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
